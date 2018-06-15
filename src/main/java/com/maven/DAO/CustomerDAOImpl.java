a92227ee61bbb0ddfb9889bddb9208223e69aa1c
liukai0219 <852738808@qq.com>
2018-06-16 00:21:05 +0900

    add update method

--- src/main/java/com/maven/DAO/CustomerDAOImpl.java
+++ src/main/java/com/maven/DAO/CustomerDAOImpl.java
@@ -6,18 +6,62 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.maven.DBUtils.DBUtils;
 import com.maven.bean.Customer;
 
 public class CustomerDAOImpl implements CustomerDAO {
-
-	public List<Customer> findCustomer() throws SQLException {
+	@Override
+	public List<Customer> findCustomer(Customer cus) throws SQLException {
+		List<Customer> list = new ArrayList<Customer>();
 		Connection conn = DBUtils.getConnection();
-		return null;
+		Statement stmt = conn.createStatement();
+		String sql = "SELECT * FROM Customer";
+		
+		
+		
+		sql = sql + ";";
+		ResultSet rs = stmt.executeQuery(sql);
+		while (rs.next()) {
+			Customer customer = new Customer();
+			customer.setId(rs.getString("id"));
+			list.add(customer);
+		}
+		return list;
 	}
 
+	@Override
+	public boolean updateCustomer(Customer cus) throws SQLException {
+		Connection conn = DBUtils.getConnection();
+		Statement stmt = conn.createStatement();
+		int isActive = 1;
+		if (cus.isActive() == false)
+			isActive = 0;
+		String datetime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss"));
+		String sql = "UPDATE Customer SET "
+				+"`active`='"+isActive+"',"
+				+"`balance`='"+cus.getBalance()+"',"
+				+"`picture`='"+cus.getPicture()+"',"
+				+"`age`='"+cus.getAge()+"',"
+				+"`eyecolor`='"+cus.getEyecolor()+"',"
+				+"`lastName`='"+cus.getLastName()+"',"
+				+"`firstName`='"+cus.getFirstName()+"',"
+				+"`company`='"+cus.getCompany()+"',"
+				+"`email`='"+cus.getEmail()+"',"
+				+"`phone`='"+cus.getPhone()+"',"
+				+"`address`='"+cus.getAddress()+"',"
+				+"`about`='"+cus.getAbout()+"',"
+				+"`favoriteFruit`='"+cus.getFavoriteFruit()+"',"
+				+"`updated`='"+datetime+"'"
+				+"WHERE "
+				+"id='"+cus.getId()+"';";
+		boolean result = stmt.execute(sql);
+		return result;
+	}
+
+	@Override
 	public boolean addCustomer(Customer cus) throws SQLException {
 		Connection conn = DBUtils.getConnection();
 		Statement stmt = conn.createStatement();
@@ -72,5 +116,9 @@ public class CustomerDAOImpl implements CustomerDAO {
 		boolean result = stmt.execute(sql);
 		return result;
 	}
+
+
+
+
 
 }
